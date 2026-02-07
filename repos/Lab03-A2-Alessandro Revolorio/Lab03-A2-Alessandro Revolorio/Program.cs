10c79e0d71659f19a254cec3f21142ba91e68097
Alessandrorev <alessandrorevolorio094@gmail.com>
2026-02-06 23:30:16 -0600

    Laboratorio 3

---
+++ repos/Lab03-A2-Alessandro Revolorio/Lab03-A2-Alessandro Revolorio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nombre del estudiante: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nombre del curso: ");
+            string curso = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Lab de Pensamiento Computacional");
+            Console.WriteLine(nombre + ", vas muy bien en el curso " + curso + ".");
+            Console.WriteLine("Nunca dejes de practicar.");
+            Console.WriteLine();
+            Console.WriteLine("Hoy es un buen día para seguir aprendiendo programación.");
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para salir...");
+
+            Console.ReadKey();
+        }
+    }
+}
