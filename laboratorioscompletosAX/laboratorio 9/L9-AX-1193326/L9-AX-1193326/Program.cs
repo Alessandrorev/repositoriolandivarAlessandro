6a9007d017ceccea4b295eda48be74f1b0d4e44b
Alessandrorev <alessandrorevolorio094@gmail.com>
2026-03-20 23:36:43 -0600

    labs9

---
+++ laboratorioscompletosAX/laboratorio 9/L9-AX-1193326/L9-AX-1193326/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("EJERCICIO 1");
+        Console.Write("Ingrese una palabra: ");
+        string palabra = Console.ReadLine();
+
+        ContarCaracteres(palabra);
+
+        Console.WriteLine("\nEJERCICIO 2");
+        int A = 4;
+        int B = 13;
+
+        Console.WriteLine("Antes:");
+        Console.WriteLine("A = " + A);
+        Console.WriteLine("B = " + B);
+
+        Intercambiar(ref A, ref B);
+
+        Console.WriteLine("Después:");
+        Console.WriteLine("A = " + A);
+        Console.WriteLine("B = " + B);
+
+        Console.WriteLine("\nEJERCICIO 3");
+
+        double precio = 48.50;
+        double descuento = 0.15;
+
+        Console.WriteLine("Precio antes: Q" + precio);
+
+        AplicarDescuento(descuento, ref precio);
+
+        Console.WriteLine("Precio después: Q" + precio);
+
+        Console.WriteLine("\nEJERCICIO 4");
+
+        int salud = 10;
+
+        mostrarSalud(salud);
+
+        recibirDanio(ref salud);
+        mostrarSalud(salud);
+
+        curar(ref salud);
+        mostrarSalud(salud);
+
+        calificarDesempeno(salud);
+    }
+
+
+    static void ContarCaracteres(string texto)
+    {
+        Console.WriteLine("Cantidad de caracteres: " + texto.Length);
+    }
+
+
+    static void Intercambiar(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+
+    static void AplicarDescuento(double porcentaje, ref double precio)
+    {
+        precio = precio - (precio * porcentaje);
+    }
+
+    static void recibirDanio(ref int salud)
+    {
+        salud -= 5;
+        if (salud < 0)
+            salud = 0;
+    }
+
+    static void curar(ref int salud)
+    {
+        salud += 3;
+        if (salud > 15)
+            salud = 15;
+    }
+
+    static void mostrarSalud(int salud)
+    {
+        if (salud >= 11)
+            Console.ForegroundColor = ConsoleColor.Green;
+        else if (salud >= 6)
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        else
+            Console.ForegroundColor = ConsoleColor.Red;
+
+        Console.WriteLine("Salud: " + salud);
+        Console.ResetColor();
+    }
+
+    static void calificarDesempeno(int salud)
+    {
+        Console.Write("Calificación: ");
+
+        if (salud == 15)
+            Console.WriteLine("S");
+        else if (salud >= 11)
+            Console.WriteLine("A");
+        else if (salud >= 6)
+            Console.WriteLine("B");
+        else
+            Console.WriteLine("C");
+    }
+}
---
+++ repos/L8-AX-1193326/L8-AX-1193326/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("===== LABORATORIO 08 =====");
+        Console.WriteLine("1 - Estadísticas de 20 números");
+        Console.WriteLine("2 - Par / Siete / ParSiete");
+        Console.WriteLine("3 - Tienda (10 clientes)");
+        Console.WriteLine("4 - Menú de números");
+        Console.WriteLine("5 - Triángulo de asteriscos");
+
+        Console.Write("Seleccione opción: ");
+        int opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                Ejercicio1();
+                break;
+            case 2:
+                Ejercicio2();
+                break;
+            case 3:
+                Ejercicio3();
+                break;
+            case 4:
+                Ejercicio4();
+                break;
+            case 5:
+                Ejercicio5();
+                break;
+            default:
+                Console.WriteLine("Opción inválida");
+                break;
+        }
+    }
+
+    static void Ejercicio1()
+    {
+        int numero;
+        int mayor = int.MinValue;
+        int menor = int.MaxValue;
+        int suma = 0;
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write("Ingrese número " + i + ": ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > mayor)
+                mayor = numero;
+
+            if (numero < menor)
+                menor = numero;
+
+            suma += numero;
+        }
+
+        double promedio = (double)suma / 20;
+
+        Console.WriteLine("Mayor: " + mayor);
+        Console.WriteLine("Menor: " + menor);
+        Console.WriteLine("Promedio: " + promedio);
+    }
+
+    static void Ejercicio2()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0 && i % 7 == 0)
+                Console.WriteLine(i + " ParSiete");
+            else if (i % 2 == 0)
+                Console.WriteLine(i + " Par");
+            else if (i % 7 == 0)
+                Console.WriteLine(i + " Siete");
+            else
+                Console.WriteLine(i);
+        }
+    }
+
+    static void Ejercicio3()
+    {
+        double compra;
+        double totalVentas = 0;
+        int clientesConDescuento = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Monto cliente " + i + ": ");
+            compra = double.Parse(Console.ReadLine());
+
+            double descuento = 0;
+
+            if (compra > 700)
+            {
+                descuento = 0.12;
+                clientesConDescuento++;
+            }
+            else if (compra > 300)
+            {
+                descuento = 0.05;
+                clientesConDescuento++;
+            }
+
+            double total = compra - (compra * descuento);
+
+            Console.WriteLine("Total pagado: $" + total);
+
+            totalVentas += total;
+        }
+
+        Console.WriteLine("Clientes con descuento: " + clientesConDescuento);
+        Console.WriteLine("Total ventas del día: $" + totalVentas);
+    }
+
+    static void Ejercicio4()
+    {
+        Console.Write("Ingrese un número: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("1 - Mostrar desde N hasta 1");
+        Console.WriteLine("2 - Múltiplos de 3");
+        Console.WriteLine("3 - Múltiplos de 5");
+
+        Console.Write("Seleccione opción: ");
+        int opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                for (int i = n; i >= 1; i--)
+                    Console.WriteLine(i);
+                break;
+
+            case 2:
+                for (int i = 1; i <= n; i++)
+                    if (i % 3 == 0)
+                        Console.WriteLine(i);
+                break;
+
+            case 3:
+                for (int i = 1; i <= n; i++)
+                    if (i % 5 == 0)
+                        Console.WriteLine(i);
+                break;
+
+            default:
+                Console.WriteLine("Opción inválida");
+                break;
+        }
+    }
+
+    static void Ejercicio5()
+    {
+        Console.Write("Ingrese número de filas: ");
+        int filas = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= filas; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
